988cbbc942ec4d3e27a9ed3371aa2e7c583d60cf
Sergey <>
2014-11-27 14:06:40 +0400

    Set Game object in Supervisor

--- src/cards/Cards.java
+++ src/cards/Cards.java
@@ -1,11 +1,12 @@
 package cards;
 
-import cards.game.DumbGame;
+import cards.game.*;
 
 public class Cards {
 
     public static void main(String[] args) {
         DumbGame game = new DumbGame();
+        Supervisor.getInstance().setGame(game);
         game.play();
     }
 }
