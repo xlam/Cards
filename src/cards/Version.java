8adc03881eeb1596423c1db4ea556d4868cea0f2
Sergey <xlamserg@gmail.com>
2014-11-30 20:20:59 +0400

    Starting version 1.1-dev

--- src/cards/Version.java
+++ src/cards/Version.java
@@ -7,8 +7,8 @@ package cards;
 public class Version {
 
     private static final String VERSION_MAJOR = "1";
-    private static final String VERSION_MINOR = "0";
-    private static final String VERSION_STAGE = "";
+    private static final String VERSION_MINOR = "1";
+    private static final String VERSION_STAGE = "dev";
 
     public static final String getVersion() {
         return VERSION_MAJOR + "." + VERSION_MINOR + "-" + VERSION_STAGE;
