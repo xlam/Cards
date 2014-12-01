95be609e2defcf6690e17e4286bfb5ee52639658
Sergey <>
2014-12-01 16:57:27 +0400

    Starting version 1.2-dev

--- src/cards/main/Version.java
+++ src/cards/main/Version.java
@@ -7,11 +7,11 @@ package cards.main;
 public class Version {
 
     private static final String VERSION_MAJOR = "1";
-    private static final String VERSION_MINOR = "1";
+    private static final String VERSION_MINOR = "2";
     private static final String VERSION_STAGE = "dev";
 
     public static final String getVersion() {
         return VERSION_MAJOR + "." + VERSION_MINOR + "-" + VERSION_STAGE;
     }
-    
+
 }
