4ccfc1e8dee0163a93de6be3f2d907308c226c54
Sergey <sergsm@serg>
2014-12-03 10:00:49 +0400

    Update Player and DumbPlayer tests.

--- test/cards/dumbgame/AllTests.java
+++ test/cards/dumbgame/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     DumbHandTest.class,
-    PlayerTest.class,
+    DumbPlayerTest.class,
     SimpleStrategyTest.class,
     MemoryStrategyTest.class,
     SupervisorTest.class
