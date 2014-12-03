05684b66f73af6b0588381859aba1e371ee05655
Sergey <xlamserg@gmail.com>
2014-12-03 21:42:36 +0300

    Starting version 1.3.1-dev.
    Added VERSION_BUILD tocken to version facility.

--- src/cards/Version.java
+++ src/cards/Version.java
@@ -7,11 +7,16 @@ package cards;
 public class Version {
 
     private static final String VERSION_MAJOR = "1";
-    private static final String VERSION_MINOR = "2";
-    private static final String VERSION_STAGE = "";
+    private static final String VERSION_MINOR = "3";
+    private static final String VERSION_BUILD = "1";
+    private static final String VERSION_STAGE = "dev";
 
     public static final String getVersion() {
-        return VERSION_MAJOR + "." + VERSION_MINOR + "-" + VERSION_STAGE;
+        return
+            VERSION_MAJOR + "." +
+            VERSION_MINOR + "." +
+            VERSION_BUILD + "-" +
+            VERSION_STAGE;
     }
 
 }
