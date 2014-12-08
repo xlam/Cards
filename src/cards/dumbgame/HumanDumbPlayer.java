6030c88cdf6270db6ee39c782b16b486abef69ac
Sergey <sergsm@serg>
2014-12-08 17:02:38 +0400

    Working on HumanDumbPlayer functionality.

--- src/cards/dumbgame/HumanDumbPlayer.java
+++ src/cards/dumbgame/HumanDumbPlayer.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -43,6 +44,17 @@ public class HumanDumbPlayer extends DumbPlayer {
         Card c = getCardMatchedInput(input); // TODO do card verification
         hand.remove(c);
         return c;
+    }
+
+    protected ArrayList<Card> getValidCardsToMove(ArrayList<Card> cardsInAction, Suit trump) {
+        if (cardsInAction.isEmpty())
+            return hand.toArrayList();
+        ArrayList<Card> validCards = new ArrayList<>();
+        for (Card c1: hand.toArrayList())
+            for (Card c2: cardsInAction)
+                if (c1.getRank().equals(c2.getRank()))
+                    validCards.add(c1);
+        return validCards;
     }
 
     private Card getCardMatchedInput(String input) {
--- test/cards/dumbgame/HumanPlayerTest.java
+++ test/cards/dumbgame/HumanPlayerTest.java
@@ -7,8 +7,10 @@
 package cards.dumbgame;
 
 import cards.common.*;
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *
@@ -21,6 +23,10 @@ public class HumanPlayerTest {
 
     @Test
     public void testHumanPlayer() {
+        Suit trump = Suit.SPADES;
+        ArrayList<Card> expected;
+        ArrayList<Card> result;
+        ArrayList<Card> cardsInAction = new ArrayList<>();
         HumanDumbPlayer human = new HumanDumbPlayer("Human");
         human.addCard(new Card(Suit.SPADES,   Rank.QUEEN));
         human.addCard(new Card(Suit.HEARTS,   Rank.TEN));
@@ -28,9 +34,21 @@ public class HumanPlayerTest {
         human.addCard(new Card(Suit.DIAMONDS, Rank.KING));
         human.addCard(new Card(Suit.SPADES,   Rank.EIGHT));
         human.addCard(new Card(Suit.CLUBS,    Rank.EIGHT));
-        human.setInput(new java.io.ByteArrayInputStream("10h".getBytes()));
-        Card beatCard = human.beat(new Card(Suit.HEARTS, Rank.NINE));
-        assertTrue(beatCard.isSameAs(new Card(Suit.HEARTS, Rank.TEN)));
+        Hand hand = human.getHand();
+        expected = hand.toArrayList();
+        result = human.getValidCardsToMove(cardsInAction, trump);
+        assertEquals(expected, result);
+        cardsInAction.add(new Card(Suit.DIAMONDS, Rank.SEVEN));
+        cardsInAction.add(new Card(Suit.DIAMONDS, Rank.EIGHT));
+        expected.clear();
+        expected.add(new Card(Suit.SPADES, Rank.EIGHT));
+        expected.add(new Card(Suit.CLUBS, Rank.EIGHT));
+        result = human.getValidCardsToMove(cardsInAction, trump);
+        assertEquals(expected, result);
+
+//        human.setInput(new java.io.ByteArrayInputStream("10h".getBytes()));
+//        Card beatCard = human.beat(new Card(Suit.HEARTS, Rank.NINE));
+//        assertTrue(beatCard.isSameAs(new Card(Suit.HEARTS, Rank.TEN)));
         // TODO add more tests with multiple cases and trump
     }
 }
