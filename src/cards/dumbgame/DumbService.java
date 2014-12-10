55940a4b282334a070e62ff6b3a4888397ef9a61
Sergey <xlamserg@gmail.com>
2014-12-10 19:30:32 +0300

    Add basic DumbService and test

---
+++ src/cards/dumbgame/DumbService.java
@@ -0,0 +1,16 @@
+package cards.dumbgame;
+
+import cards.common.Card;
+import cards.common.Suit;
+
+/**
+ *
+ * @author Kus
+ */
+public class DumbService {
+
+    public int getValueOf(Card card, Suit trump) {
+        return 1;
+    }
+
+}
---
+++ test/cards/dumbgame/DumbServiceTest.java
@@ -0,0 +1,46 @@
+package cards.dumbgame;
+
+import cards.common.Card;
+import cards.common.Rank;
+import cards.common.Suit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kus
+ */
+public class DumbServiceTest {
+
+    private DumbService service;
+    private Suit trump;
+
+    public DumbServiceTest() {
+    }
+
+    @Before
+    public void setUp() {
+        service = new DumbService();
+        trump = Suit.SPADES;
+    }
+
+    @After
+    public void tearDown() {
+        service = null;
+    }
+
+    @Test
+    public void testAssertCardsValues() {
+        Card card1 = new Card(Suit.CLUBS, Rank.SIX);
+        Card card2 = new Card(Suit.CLUBS, Rank.SEVEN);
+        Card card3 = new Card(Suit.CLUBS, Rank.TEN);
+        Card card4 = new Card(Suit.HEARTS, Rank.SIX);
+        Card card5 = new Card(Suit.DIAMONDS, Rank.SEVEN);
+        Card card6 = new Card(Suit.SPADES, Rank.SIX);
+        Card card7 = new Card(Suit.SPADES, Rank.TEN);
+        assertTrue(service.getValueOf(card1, trump) < service.getValueOf(card2, trump));
+    }
+
+}
