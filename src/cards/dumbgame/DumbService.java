2559410e32ae9f2a491086faa1c2b8d7bd35b712
Sergey <xlamserg@gmail.com>
2014-12-10 20:00:49 +0300

    Make servise test pass for the first time.

--- src/cards/dumbgame/DumbService.java
+++ src/cards/dumbgame/DumbService.java
@@ -1,7 +1,7 @@
 package cards.dumbgame;
 
-import cards.common.Card;
-import cards.common.Suit;
+import cards.common.*;
+import java.util.HashMap;
 
 /**
  *
@@ -9,8 +9,27 @@ import cards.common.Suit;
  */
 public class DumbService {
 
+    private HashMap<Rank, Integer> VALUES;
+    private final int delta = 10;
+
+    public DumbService() {
+        VALUES = new HashMap();
+        VALUES.put(Rank.SIX,    1);
+        VALUES.put(Rank.SEVEN,  2);
+        VALUES.put(Rank.EIGHT,  3);
+        VALUES.put(Rank.NINE,   4);
+        VALUES.put(Rank.TEN,    5);
+        VALUES.put(Rank.JACK,   6);
+        VALUES.put(Rank.QUEEN,  7);
+        VALUES.put(Rank.KING,   8);
+        VALUES.put(Rank.ACE,    9);
+    }
+
     public int getValueOf(Card card, Suit trump) {
-        return 1;
+        int d = 0;
+        if (card.getSuit().equals(trump))
+            d += delta;
+        return VALUES.get(card.getRank()) + d;
     }
 
 }
