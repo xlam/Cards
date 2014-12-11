ca566fc93958cffe749867f54deaffac5efb987e
Sergey <sergsm@serg>
2014-12-11 17:00:10 +0400

    Temp commit to continue work later (getPairs() is buggy).

--- src/cards/dumbgame/AdvancedStrategy.java
+++ src/cards/dumbgame/AdvancedStrategy.java
@@ -30,4 +30,29 @@ public class AdvancedStrategy extends AbstractStrategy {
         return beatCard;
     }
 
+    protected List getPairs(List<Card> cards) {
+        List pairs = new ArrayList();
+        for (Rank rank: getRanks(cards)) {
+            int count = 0;
+            Card card = null;
+            for (Card c: cards) {
+                card = c;
+                if (c.getRank().equals(rank))
+                    count++;
+            }
+            if (count > 1)
+                pairs.add(card);
+        }
+        return pairs;
+    }
+
+    private List<Rank> getRanks(List<Card> cards) {
+        List ranks = new ArrayList();
+        for (Card c: cards) {
+            Rank rank = c.getRank();
+            if (!(ranks.contains(rank)))
+                ranks.add(rank);
+        }
+        return ranks;
+    }
 }
--- test/cards/dumbgame/AdvancedStrategyTest.java
+++ test/cards/dumbgame/AdvancedStrategyTest.java
@@ -1,9 +1,14 @@
 package cards.dumbgame;
 
+import cards.common.Card;
+import cards.common.Rank;
+import cards.common.Suit;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -11,20 +16,37 @@ import static org.junit.Assert.*;
  */
 public class AdvancedStrategyTest {
 
+    private Suit trump;
+    private AdvancedStrategy strategy;
+
     public AdvancedStrategyTest() {
     }
 
     @Before
     public void setUp() {
+        strategy = new AdvancedStrategy();
+        trump = Suit.SPADES;
     }
 
     @After
     public void tearDown() {
+        strategy = null;
+        trump = null;
     }
 
     @Test
     public void testGetPairs() {
-        
+        List<Card> cards = new ArrayList();
+        cards.add(new Card(Suit.CLUBS,     Rank.SIX));
+        cards.add(new Card(Suit.DIAMONDS,  Rank.SIX));
+        cards.add(new Card(Suit.CLUBS,     Rank.SEVEN));
+        cards.add(new Card(Suit.CLUBS,     Rank.EIGHT));
+        cards.add(new Card(Suit.SPADES,    Rank.SIX));
+        cards.add(new Card(Suit.SPADES,    Rank.SEVEN));
+        List pairs = strategy.getPairs(cards);
+        System.out.println(pairs);
+        assertEquals(2, pairs.size());
+        fail("getPairs() is buggy");
     }
 
 }
