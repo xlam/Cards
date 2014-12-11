378ae63f86024ca6911c78f464cec535970e9de6
Sergey <sergsm@serg>
2014-12-11 15:26:04 +0400

    Add test for AdvancedStrategy.

---
+++ test/cards/dumbgame/AdvancedStrategyTest.java
@@ -0,0 +1,30 @@
+package cards.dumbgame;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sergey
+ */
+public class AdvancedStrategyTest {
+
+    public AdvancedStrategyTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetPairs() {
+        
+    }
+
+}
