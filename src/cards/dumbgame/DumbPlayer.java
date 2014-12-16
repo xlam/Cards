57dcfd086380526f48d51bc06c6576025493f506
Sergey <sergsm@serg>
2014-12-16 14:32:21 +0400

    Remove dead code.

--- src/cards/dumbgame/DumbPlayer.java
+++ src/cards/dumbgame/DumbPlayer.java
@@ -3,7 +3,6 @@ package cards.dumbgame;
 import cards.common.Card;
 import cards.common.Player;
 import cards.common.Suit;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,27 +14,12 @@ public abstract class DumbPlayer extends Player {
     public abstract Card beat(Card card, Suit trump);
     public abstract Strategy getStrategy();
 
-    protected Supervisor supervisor;
     protected DumbService service;
-
-    protected Supervisor getSupervisor() {
-        if (null == supervisor)
-            supervisor = Supervisor.getInstance();
-        return supervisor;
-    }
 
     protected DumbService getService() {
         if (null == service)
             service = new DumbService();
         return service;
-    }
-
-    protected List getCardsInAction() {
-        return getSupervisor().getCardsInAction();
-    }
-
-    protected Suit getTrumpSuit() {
-        return getSupervisor().getTrumpSuit();
     }
 
     @Override
